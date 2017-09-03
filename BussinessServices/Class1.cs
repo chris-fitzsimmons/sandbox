ca61242ea98a1029ee1aaa773c53b1bbdf97ba1c
cfitzsimmons <cfitzsimmons@WIN7GAMER>
2017-09-03 16:32:36 -0400

    setup

---
+++ BussinessServices/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BussinessServices
+{
+    public class Class1
+    {
+    }
+}
